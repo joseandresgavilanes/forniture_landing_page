1f37e31375f1c92915f4170714f178df32d50e41
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2022-10-29 22:00:51 -0500

    added

---
+++ src/components/NavMobile.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+// import data
+import { navigation } from "../data";
+
+const NavMobile = () => {
+  return (
+    <nav className="bg-white shadow-2xl w-full h-full">
+      <ul className="text-center h-full flex flex-col items-center justify-center gap-y-6">
+        {navigation.map((item, index) => {
+          return (
+            <li key={index}>
+              <button
+                className="text-xl font-medium capitalize"
+                href={item.href}
+              >
+                {item.name}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavMobile;
